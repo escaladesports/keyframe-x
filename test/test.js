73efa4d58e5388af2080ad58a4d7b460f0caace8
Kennedy Rose <kennedy@kennedyrose.com>
2018-04-11 16:05:43 -0500

    Number tests

--- test/test.js
+++ test/test.js
@@ -1,40 +1,42 @@
 import { expect } from 'chai'
-import objectTween from '../src'
+import tween from '../src'
 
 describe('Object tween', () => {
+	it('Should tween a number', () => {
+		const tests = [
+			[0, 100, 50],
+			[100, 0, 50],
+			[100, 200, 150],
+			[200, 100, 150],
+			[-50, 50, 0],
+			[50, -50, 0],
+		]
+		tests.forEach(nums => {
+			expect(tween(nums[0], nums[1])(.5)).to.equal(nums[2])
+		})
+	})
+
+	it('Should tween a string', () => {
+		const state = tween('test(0)string', 'test(100)string')(.5)
+		expect(state).to.equal('test(50)string')
+	})
+
 	it('Should deep tween an object', () => {
 		const from = {
 			a: 0,
-			b: 100,
 			hundred: {
 				c: 100,
-				d: 200,
-			},
-			negative: {
-				e: -50,
-				f: 50,
-			},
+			}
 		}
 		const to = {
 			a: 100,
-			b: 0,
 			hundred: {
 				c: 200,
-				d: 100,
-			},
-			negative: {
-				e: 50,
-				f: -50,
 			}
 		}
-		const tween = objectTween(from, to)
-		const state = tween(.5)
+		const state = tween(from, to)(.5)
 		expect(state.a).to.equal(50)
-		expect(state.b).to.equal(50)
 		expect(state.hundred.c).to.equal(150)
-		expect(state.hundred.d).to.equal(150)
-		expect(state.negative.e).to.equal(0)
-		expect(state.negative.f).to.equal(0)
 	})
 
 	it('Should deep tween an array', () => {
@@ -46,8 +48,7 @@ describe('Object tween', () => {
 			[ 100 ],
 			0
 		]
-		const tween = objectTween(from, to)
-		const state = tween(.5)
+		const state = tween(from, to)(.5)
 		expect(state[0][0]).to.equal(50)
 		expect(state[1]).to.equal(50)
 	})
